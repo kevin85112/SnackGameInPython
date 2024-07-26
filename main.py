0b07d40f126dd8eb1550077e8af6c3ca0622dae3
kevin <kevin85112@gmail.com>
2024-07-27 02:32:35 +0800

    update

--- main.py
+++ main.py
@@ -1,52 +1,101 @@
 import tkinter as tk
-from snake_game import SnakeGame, Sign
-
-# ■□※◎
-
-def disable_selection(event):
-    return "break"
+from snake_game import SnakeGame, Sign, Direction
 
 
-def write_text(x, y, str):
-    text_box.config(state=tk.NORMAL)
-    pos = f"{y + 1}.{x}"
-    print(pos)
-    text_box.delete(pos)
-    text_box.insert(pos, str)
-    text_box.config(state=tk.DISABLED)
+def draw_clear(x, y):
+    canvas.create_rectangle(x * snake_game.cell_size(),
+                            y * snake_game.cell_size(),
+                            (x + 1) * snake_game.cell_size(),
+                            (y + 1) * snake_game.cell_size(),
+                            fill="black")
+
+
+def draw_rect_fill(x, y, color):
+    draw_clear(x, y)
+    canvas.create_rectangle(x * snake_game.cell_size(),
+                            y * snake_game.cell_size(),
+                            (x + 1) * snake_game.cell_size(),
+                            (y + 1) * snake_game.cell_size(),
+                            fill=color)
+
+
+def draw_rect(x, y, color):
+    draw_clear(x, y)
+    canvas.create_rectangle(x * snake_game.cell_size() + 1,
+                            y * snake_game.cell_size() + 1,
+                            (x + 1) * snake_game.cell_size() - 1,
+                            (y + 1) * snake_game.cell_size() - 1,
+                            outline=color, width=1)
+
+
+def draw_circle(x, y, color):
+    draw_clear(x, y)
+    canvas.create_oval(x * snake_game.cell_size(),
+                       y * snake_game.cell_size(),
+                       (x + 1) * snake_game.cell_size(),
+                       (y + 1) * snake_game.cell_size(),
+                       fill=color)
+
 
 def on_update(x, y, sign):
     if (sign == Sign.WALL):
-        write_text(x, y, "※")
+        draw_rect_fill(x, y, "gray")
     elif (sign == Sign.SNACK_BODY):
-        write_text(x, y, "■")
+        draw_rect(x, y, "green")
     elif (sign == Sign.SNACK_HEAD):
-        write_text(x, y, "□")
+        draw_rect_fill(x, y, "green")
     elif (sign == Sign.POINT):
-        write_text(x, y, "◎")
+        draw_circle(x, y, "brown")
+    elif (sign == Sign.NONE):
+        draw_clear(x, y)
+
+
+def on_end():
+    pass
+
+
+def on_key_press(event):
+    if event.keysym == "Up":
+        snake_game.send_dir(Direction.UP)
+    elif event.keysym == "Down":
+        snake_game.send_dir(Direction.DOWN)
+    elif event.keysym == "Left":
+        snake_game.send_dir(Direction.LEFT)
+    elif event.keysym == "Right":
+        snake_game.send_dir(Direction.RIGHT)
+
+
+def perform_action():
+    if (snake_game.is_start()):
+        snake_game.next()
+        root.after(100, perform_action)
+
 
 root = tk.Tk()
 root.title("Snack game")
 root.geometry("600x600")
+root.configure(bg="black")
 
-snake_game = SnakeGame()
+snake_game = SnakeGame(width=32, height=32)
 
-text_box = tk.Text(root, height=snake_game.height(), width=snake_game.width(), cursor="arrow",
-                   highlightthickness=0, borderwidth=0, wrap="none", bg=root.cget("bg"))
-text_box.pack(fill="both", expand=True, padx=10, pady=10)
+canvas_width = snake_game.width() * snake_game.cell_size()
+canvas_height = snake_game.height() * snake_game.cell_size()
 
-text_box.bind("<Button-1>", disable_selection)
-text_box.bind("<B1-Motion>", disable_selection)
-text_box.bind("<Key>", disable_selection)
+root.geometry(f"{canvas_width}x{canvas_height}")
 
-for row in range(snake_game.height()):
-    for column in range(snake_game.width()):
-        text_box.insert(tk.END, "　")
-    text_box.insert(tk.END, "\n")
-
-text_box.config(state=tk.DISABLED)
+canvas = tk.Canvas(root, width=canvas_width,
+                   height=canvas_height, highlightthickness=0, borderwidth=0, bg='black')
+canvas.pack(expand=True)
 
 snake_game.register_update(on_update)
+snake_game.register_update(on_update)
 snake_game.start()
+
+root.after(2000, perform_action)
+
+root.bind("<Up>", on_key_press)
+root.bind("<Down>", on_key_press)
+root.bind("<Left>", on_key_press)
+root.bind("<Right>", on_key_press)
 
 root.mainloop()
--- snake_game.py
+++ snake_game.py
@@ -1,47 +1,182 @@
-from typing import Callable
+from typing import Callable, Tuple, List
 from pyee import EventEmitter
 from enum import Enum, auto
+from random import randint
 
 
 class Sign(Enum):
+    NONE = auto()
     WALL = auto()
     SNACK_HEAD = auto()
     SNACK_BODY = auto()
     POINT = auto()
 
 
+class Direction(Enum):
+    LEFT = auto()
+    UP = auto()
+    RIGHT = auto()
+    DOWN = auto()
+
+
 class SnakeGame:
-    def __init__(self, snack_length=4, width=32, height=32):
-        self.__emitter = EventEmitter()
-        self.__event_update = "update"
+    @staticmethod
+    def __calc_pos(pos: Tuple[int, int], dir: Direction) -> Tuple[int, int]:
+        x = pos[0]
+        y = pos[1]
+        if (dir == Direction.LEFT):
+            return (x - 1, y)
+        elif (dir == Direction.UP):
+            return (x, y - 1)
+        elif (dir == Direction.RIGHT):
+            return (x + 1, y)
+        else:
+            return (x, y + 1)
+
+    @staticmethod
+    def __reverse_dir(dir: Direction) -> Direction:
+        if (dir == Direction.LEFT):
+            return Direction.RIGHT
+        elif (dir == Direction.UP):
+            return Direction.DOWN
+        elif (dir == Direction.RIGHT):
+            return Direction.LEFT
+        else:
+            return Direction.UP
+
+    def __init__(self, cell_size=20, snack_length=4, width=32, height=32, num_of_points=3):
+        self.__cell_size = cell_size
+        self.__snack_length = snack_length
         self.__width = width
         self.__height = height
-        self.__snack_length = snack_length
-        self.__current_snack_length = snack_length
+        self.__num_of_points = num_of_points
 
-    def width(self):
-        return self.__width
-    
-    def height(self):
-        return self.__height
-    
+        self.__is_start = False
+        self.__is_end = False
+        self.__enable_send_dir = False
+        self.__emitter = EventEmitter()
+        self.__event_update = "update"
+        self.__event_end = "end"
+        self.__next_dir = Direction.RIGHT
+
+    def __raise_update(self, pos: Tuple[int, int], sign: Sign) -> None:
+        self.__emitter.emit(self.__event_update, pos[0], pos[1], sign)
+
+    def __raise_end(self) -> None:
+        self.__is_end = True
+        self.__emitter.emit(self.__event_end)
+
     def register_update(self, listener: Callable[[int, int, Sign], None]):
         self.__emitter.on(self.__event_update, listener)
 
-    def start(self):
-        self.__current_snack_length = self.__snack_length
+    def register_end(self, listener: Callable[[], None]):
+        self.__emitter.on(self.__event_end, listener)
 
+    def cell_size(self):
+        return self.__cell_size
+
+    def width(self):
+        return self.__width
+
+    def height(self):
+        return self.__height
+
+    def is_start(self):
+        return self.__is_start
+
+    def is_end(self):
+        return self.__is_end
+
+    def get_walls(self) -> set[Tuple[int, int]]:
+        result = set()
         for row in [0, self.__height - 1]:
             for column in range(self.__width):
-                self.__emitter.emit(self.__event_update, column, row, Sign.WALL)
+                result.add((column, row))
 
         for row in range(1, self.__height - 1):
-            self.__emitter.emit(self.__event_update, 0, row, Sign.WALL)
-            self.__emitter.emit(self.__event_update, self.__width - 1, row, Sign.WALL)
+            result.add((0, row))
+            result.add((self.__width - 1, row))
+        return result
 
-        for i in range(self.__snack_length - 1):
-            self.__emitter.emit(self.__event_update, 3 + i, self.__height - 4, Sign.SNACK_BODY)
+    def get_bodys(self) -> List[Tuple[int, int]]:
+        list = []
+        pos = self.__current_pos
+        for dir in self.__current_body:
+            pos = SnakeGame.__calc_pos(pos, dir)
+            list.append(pos)
+        return list
 
-        self.__emitter.emit(self.__event_update, 3 + self.__snack_length - 1, self.__height - 4, Sign.SNACK_HEAD)
-        
-        
+    def __create_wall(self):
+        for pos in self.__current_walls:
+            self.__raise_update(pos, Sign.WALL)
+
+    def __create_snack(self):
+        pos = self.__current_pos
+        self.__raise_update(pos, Sign.SNACK_HEAD)
+        for pos in self.get_bodys():
+            self.__raise_update(pos, Sign.SNACK_BODY)
+
+    def __create_point(self):
+        remain = set()
+        for x in range(self.__width):
+            for y in range(self.__height):
+                remain.add((x, y))
+
+        remain = remain - {self.__current_pos} - set(self.get_bodys()) - \
+            self.__current_walls - self.__current_points
+
+        index = randint(0, len(remain) - 1)
+        pos = list(remain)[index]
+        self.__current_points.add(pos)
+        self.__raise_update(pos, Sign.POINT)
+
+    def __move_snack(self, dir: Direction):
+        newPos = SnakeGame.__calc_pos(self.__current_pos, dir)
+        self.__raise_update(newPos, Sign.SNACK_HEAD)
+        self.__raise_update(self.__current_pos, Sign.SNACK_BODY)
+        self.__current_body.insert(0, SnakeGame.__reverse_dir(dir))
+        self.__current_pos = newPos
+
+    def __pop_snack(self):
+        body = self.get_bodys()
+        self.__raise_update(body[len(body) - 1], Sign.NONE)
+        self.__current_body.pop()
+
+    def start(self):
+        self.__current_pos = (self.__snack_length + 2, self.__height - 4)
+        self.__current_body = []
+        self.__current_points = set()
+        self.__current_dir = Direction.RIGHT
+        self.__current_walls = self.get_walls()
+        self.__next_dir = Direction.RIGHT
+        for _ in range(self.__snack_length - 1):
+            self.__current_body.append(Direction.LEFT)
+
+        self.__create_wall()
+        self.__create_snack()
+        for _ in range(self.__num_of_points):
+            self.__create_point()
+
+        self.__enable_send_dir = True
+        self.__is_end = False
+        self.__is_start = True
+
+    def next(self):
+        if (not self.__is_start or self.__is_end):
+            return
+        self.__move_snack(self.__next_dir)
+        self.__current_dir = self.__next_dir
+        if (self.__current_pos in self.__current_points):
+            self.__current_points.remove(self.__current_pos)
+            self.__create_point()
+        elif (self.__current_pos in self.__current_walls
+              or self.__current_pos in self.get_bodys()):
+            self.__raise_end()
+        else:
+            self.__pop_snack()
+
+    def send_dir(self, dir: Direction) -> None:
+        if (not self.__is_start or self.__is_end or not self.__enable_send_dir):
+            return
+        if (dir != SnakeGame.__reverse_dir(self.__current_dir)):
+            self.__next_dir = dir
